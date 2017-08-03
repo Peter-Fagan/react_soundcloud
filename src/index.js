2d0e4b9b6435f6b8cd7b6b48672f911da06d4916
Peter-Fagan <peter_fagan@hotmail.com>
2017-08-03 14:39:59 +1000

    Finish setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-console.log("My React setup");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "My react setup";
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
 
 module.hot.accept();
