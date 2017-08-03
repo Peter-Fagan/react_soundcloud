370b3c0b42197c4a0e9c1c8d5518e2ea4bfd1124
Peter-Fagan <peter_fagan@hotmail.com>
2017-08-03 14:50:23 +1000

    Create base page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = "My react setup";
+const tracks = [
+  {
+    title: 'some track'
+  },
+  {
+    title: 'some other track'
+  }
+];
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div>
+    {
+      tracks.map((track, key) => {
+        return <div className="track" key={key}>{track.title}</div>
+      })
+    }
+  </div>,
   document.getElementById('app')
 );
 
