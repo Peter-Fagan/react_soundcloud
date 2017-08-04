5b982fcc31c963998c423187c50bd1c3e7d9bfc0
Peter-Fagan <peter_fagan@hotmail.com>
2017-08-04 11:19:38 +1000

    Tidy code

--- test/setup.js
+++ test/setup.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { expect } from 'chai';
-import { JSDOM } from 'jsdom';
+import {expect} from 'chai';
+import {JSDOM} from 'jsdom';
 
 const doc = new JSDOM('<html><body></body></html>');
-const { window } = doc;
+const {window} = doc;
 
 function copyProps(src, target) {
-const props = Object.getOwnPropertyNames(src)
-.filter(prop => typeof target[prop] === 'undefined')
-.map(prop => Object.getOwnPropertyDescriptor(src, prop));
-Object.defineProperties(target, props);
+  const props = Object.getOwnPropertyNames(src)
+  .filter(prop => typeof target[prop] === 'undefined')
+  .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
 }
 
 global.window = window;
 global.document = window.document;
 global.navigator = {
-userAgent: 'node.js',
+  userAgent: 'node.js',
 };
 copyProps(window, global);
 
