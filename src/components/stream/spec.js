225a1fe1878722b319297dd2ea1ba661086c420c
Peter-Fagan <peter_fagan@hotmail.com>
2017-08-08 13:46:07 +1000

    refactoring

--- src/components/stream/spec.js
+++ src/components/stream/spec.js
@@ -1,16 +1,14 @@
-import Stream from './Stream';
-import { shallow } from 'enzyme';
+import Stream from './presenter';
+import {shallow} from 'enzyme';
 
 describe('Stream', () => {
-
   const props = {
     tracks: [{title: 'x'}, {title: 'y'}],
   };
 
   it('shows two elements', () => {
-    const element = shallow(<Stream { ...props } />);
+    const element = shallow(<stream {...props} />);
 
     expect(element.find('.track')).to.have.length(2);
   });
-
 });
